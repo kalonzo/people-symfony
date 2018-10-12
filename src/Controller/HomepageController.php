52dee9a4653dbf0c37c075197ea1b55ec1f9aa62
Kenny ALonzo <kenny.alonzo@lab4tech.ch>
2018-10-12 16:18:52 +0200

    Ajout du controller de la page home + route du controller routes.yaml

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/homepage", name="homepage")
+     */
+    public function index()
+    {
+     
+        //Appele du composant en tête
+        //
+        //Insertion du composant homepage
+        //
+        //Apell du module ce création des candidats 
+        //
+        //Appele du composant pied de page
+        
+        
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+}
